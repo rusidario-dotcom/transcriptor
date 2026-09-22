610485d15af0eafccbbea455fccfffec9a4db7d9
Dario <dariomira009@gmail.com>
2026-09-22 11:58:34 +0200

    secondo commit video isaac

--- transcriptor-script.py
+++ transcriptor-script.py
@@ -1,5 +1,5 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
-video_id = "2WSJoDz5uXY"  # sostituisci con l'ID del video (la parte dopo "v=" nell'URL)
+video_id = "3UPSUAH_0kM"  # sostituisci con l'ID del video (la parte dopo "v=" nell'URL)
 transcript = YouTubeTranscriptApi().fetch(video_id)
 print(" ".join(snippet.text for snippet in transcript))
